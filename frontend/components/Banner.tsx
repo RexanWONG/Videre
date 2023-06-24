4171a7cbad470015181bdb87daf242f85059dd4c
Donghwui <donghwuikim0102@gmail.com>
2023-06-24 14:44:58 -0400

    Changed the Tab Title

--- frontend/components/Banner.tsx
+++ frontend/components/Banner.tsx
@@ -5,7 +5,7 @@ import BannerBg from "../assets/banner-bg.jpg";
 const Banner: NextPage = () => {
   return (
     <div
-      className="rounded-lg bg-[url('../assets/banner-bg.jpg')] bg-cover bg-center overflow-hidden bg-black"
+      className="rounded-xl bg-[url('../assets/banner-bg.jpg')] bg-cover bg-center overflow-hidden bg-black"
       style={{ height: "200px" }}
     >
       <div className="p-4 pl-8 pr-8 text-right">
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
-import type {NextPage} from 'next';
-import {Banner,Card} from '../components';
+import type { NextPage } from 'next';
+import { Banner, Card } from '../components';
+import Head from 'next/head';
 
-const Home : NextPage = () => {
+const Home: NextPage = () => {
   return (
-    <div className='flex flex-col '>
-    <Banner />
+    <div className="flex flex-col">
+      <Head>
+        <title>Videre™</title>
+      </Head>
+      <Banner />
     </div>
   );
-}
+};
 
 export default Home;
