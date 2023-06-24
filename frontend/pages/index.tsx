f023b71cfcb3881ee3c2b8a973a4129020ebb33e
MattWong-ca <matthew.yt.wong@gmail.com>
2023-06-24 12:16:27 -0400

    Add MetaMask SDK

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,12 +1,55 @@
-import type {NextPage} from 'next'
+import { MetaMaskSDK } from '@metamask/sdk';
+import React, { useEffect, useState } from "react";
 
-import {Navbar} from "../components";
+import { Navbar } from "../components";
+
 const Home: NextPage = () => {
+  const [currentAccount, setCurrentAccount] = useState("");
+
+  const options = {
+    dappMetadata: { name: "My Dapp", url: "https://mydapp.com" },
+    injectProvider: true
+  };
+
+  const MMSDK = new MetaMaskSDK(options);
+  const ethereum = MMSDK.getProvider(); // You can also access via window.ethereum
+
+  const connectWallet = async () => {
+    try {
+      // Request user accounts from MetaMask
+      const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+
+      // Update the current account state variable
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.error("Error connecting wallet:", error);
+    }
+  };
+
+  useEffect(() => {
+    const checkMetaMask = async () => {
+      if (ethereum && ethereum.isConnected()) {
+        // Request the current accounts from MetaMask
+        const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+        // Update the current account state variable
+        setCurrentAccount(accounts[0]);
+      }
+    };
+
+    checkMetaMask();
+  }, []);
+
   return (
     <div>
-      Videre
+      <h1>Videre</h1>
+      {!currentAccount ? (
+        <button onClick={connectWallet}>Connect Wallet</button>
+      ) : (
+        <p>Connected Account: {currentAccount}</p>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
