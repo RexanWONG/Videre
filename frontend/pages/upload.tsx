463420b228a2a0d61920f836499e90518e287379
Donghwui <donghwuikim0102@gmail.com>
2023-06-24 17:20:42 -0400

    Upload File Created

---
+++ frontend/pages/upload.tsx
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from "react";
+import { Banner, Card } from "../components";
+import Head from "next/head";
+
+const Upload = () => {
+  return (
+<h1>Upload</h1>  );
+};
+
+export default Upload;
