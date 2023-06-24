5de29558325697757f8a243971f7631789e9d3bf
Donghwui <donghwuikim0102@gmail.com>
2023-06-24 12:54:31 -0400

    New Banner Component & Fixed Bugs with MetaMask

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -1,10 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import Link from "next/link";
 import Image from "next/image";
 import Logo from "../assets/videre-logo.png";
 import { IoMdAdd } from "react-icons/io";
+import { MetaMaskSDK } from '@metamask/sdk';
+
 
 const Navbar = () => {
+   const [currentAccount, setCurrentAccount] = useState("");
+
+   const options = {
+     dappMetadata: { name: "My Dapp", url: "https://mydapp.com" },
+     injectProvider: true
+   };
+ 
+   const MMSDK = new MetaMaskSDK(options);
+   const ethereum = MMSDK.getProvider(); // You can also access via window.ethereum
+ 
+   const connectWallet = async () => {
+     try {
+       // Request user accounts from MetaMask
+       const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+ 
+       // Update the current account state variable
+       setCurrentAccount(accounts[0]);
+     } catch (error) {
+       console.error("Error connecting wallet:", error);
+     }
+   };
+ 
+   useEffect(() => {
+     const checkMetaMask = async () => {
+       if (ethereum && ethereum.isConnected()) {
+         // Request the current accounts from MetaMask
+         const accounts = await ethereum.request({ method: 'eth_accounts' });
+ 
+         // Update the current account state variable
+         setCurrentAccount(accounts[0]);
+       }
+     };
+ 
+     checkMetaMask();
+   }, []);
+
+   
   return (
     <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
       <Link href="/">
@@ -14,18 +53,22 @@ const Navbar = () => {
       </Link>
 
       <div className="flex items-center space-x-2">
-        <button className=" border-2 px-2 md:px-6 py-2 md:py-3 bg-blue-400 text-white text-md font-semibold flex items-center gap-2 rounded-full hover:scale-110 hover:bg-blue-500 hover:text-white transition-all duration-200">
-          <span><h1 className="font-raleway">Connect Wallet</h1></span>
-        </button>
+      {!currentAccount ? (
+        <button className=" border-2 px-2 md:px-6 py-2 md:py-3 bg-blue-400 text-white text-md font-semibold flex items-center gap-2 rounded-full hover:scale-110 hover:bg-blue-500 hover:text-white transition-all duration-200" 
+        onClick={connectWallet}>Connect Wallet</button>
+      ) : (
+        <p>Connected Account: {currentAccount}</p>
+      )
+      
+      }
       </div>
+      </div>)}
+
       {/* <Link href="/upload">
         <button className="border-2 px-2 md:px-4 text-md font-semibold flex items-center gap-2">
           <IoMdAdd className="text-xl" />{" "}
           <span className="hidden md:block">Upload</span>
         </button>
       </Link> */}
-    </div>
-  );
-};
 
 export default Navbar;
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,55 +1,3 @@
-<<<<<<< Updated upstream
-import { MetaMaskSDK } from '@metamask/sdk';
-import React, { useEffect, useState } from "react";
-
-import { Navbar } from "../components";
-
-const Home: NextPage = () => {
-  const [currentAccount, setCurrentAccount] = useState("");
-
-  const options = {
-    dappMetadata: { name: "My Dapp", url: "https://mydapp.com" },
-    injectProvider: true
-  };
-
-  const MMSDK = new MetaMaskSDK(options);
-  const ethereum = MMSDK.getProvider(); // You can also access via window.ethereum
-
-  const connectWallet = async () => {
-    try {
-      // Request user accounts from MetaMask
-      const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
-
-      // Update the current account state variable
-      setCurrentAccount(accounts[0]);
-    } catch (error) {
-      console.error("Error connecting wallet:", error);
-    }
-  };
-
-  useEffect(() => {
-    const checkMetaMask = async () => {
-      if (ethereum && ethereum.isConnected()) {
-        // Request the current accounts from MetaMask
-        const accounts = await ethereum.request({ method: 'eth_accounts' });
-
-        // Update the current account state variable
-        setCurrentAccount(accounts[0]);
-      }
-    };
-
-    checkMetaMask();
-  }, []);
-
-  return (
-    <div>
-      <h1>Videre</h1>
-      {!currentAccount ? (
-        <button onClick={connectWallet}>Connect Wallet</button>
-      ) : (
-        <p>Connected Account: {currentAccount}</p>
-      )}
-=======
 import React from 'react';
 import type {NextPage} from 'next';
 import {Banner} from '../components';
@@ -58,13 +6,8 @@ const Home : NextPage = () => {
   return (
     <div className='flex flex-col gap-10 h-full'>
     <Banner />
->>>>>>> Stashed changes
     </div>
   );
-};
+}
 
-<<<<<<< Updated upstream
 export default Home;
-=======
-export default Home;
->>>>>>> Stashed changes
