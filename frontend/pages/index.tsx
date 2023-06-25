b6b393147b614c51a0f0c309b41e9d0d716edda7
Donghwui <donghwuikim0102@gmail.com>
2023-06-25 06:30:58 -0400

    New Logo

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -2,16 +2,19 @@ import React from 'react';
 import type { NextPage } from 'next';
 import { Banner, Card, Content } from '../components';
 import Head from 'next/head';
+import Logo from '../assets/videre-logo.png';
 
 const Home: NextPage = () => {
+  const faviconUrl = Logo.src;
+  
   return (
     <div className="flex flex-col">
       <Head>
         <title>Videre™</title>
+        <link rel="icon" href={faviconUrl} />
       </Head>
       <Banner />
       <Content />
-      
     </div>
   );
 };
