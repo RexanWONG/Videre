1a04c91a494024f495eb07ba3d04db5d9389ad01
RexanWONG <96183717+RexanWONG@users.noreply.github.com>
2023-06-25 03:38:27 -0400

    refactor

--- frontend/pages/match.tsx
+++ frontend/pages/match.tsx
@@ -1,8 +1,44 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { ethers } from "ethers";
+import abi from "../components/data/Videre.json";
 
 const match = () => {
+  const contractAddress = "0x0784405c4438fc61f013fD00Eaabb1962c5952e9";
+  const contractABI = abi.abi;
+  const [videreContract, setVidereContract] = useState(null);
+
+  const getUserOwnedAds = async () => {
+    try {
+        let ads = []
+        let numOfVideos = await videreContract.getNumbers();
+        const numberOfVideos = numOfVideos[1].toString(); 
+    } catch (error) {
+        
+    }
+  }
+
+  useEffect(() => {
+    const initEthereum = async () => {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const videreContract = new ethers.Contract(
+        contractAddress,
+        contractABI,
+        signer
+      );
+      setVidereContract(videreContract);
+    };
+
+    initEthereum();
+  }, []);
+
   return (
-    
+    <div className='flex flex-col items-center justify-center'>
+        <h1 className='text-4xl font-bold'>
+           Ads with Content
+        </h1>
+        <p>Connect your ads with user content.  Get recgonition, stake some MATIC</p>
+    </div>
   )
 }
 
